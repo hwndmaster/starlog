a114ef8eff84612bf9ca0937160d9766b5de4827
hwndmaster <5339929+hwndmaster@users.noreply.github.com>
2022-12-05 21:26:09 -0500

    Log artifacts colorization

--- Starlog.UI/ViewModels/LogItemViewModel.cs
+++ Starlog.UI/ViewModels/LogItemViewModel.cs
@@ -1,4 +1,6 @@
+using System.Text.RegularExpressions;
 using System.Windows.Documents;
+using System.Windows.Media;
 using Genius.Atom.UI.Forms;
 using Genius.Starlog.Core.LogFlow;
 
@@ -10,7 +12,7 @@ public interface ILogItemViewModel : IViewModel
     FlowDocument Artifacts { get; }
 }
 
-public sealed class LogItemViewModel : ViewModelBase, ILogItemViewModel
+public sealed partial class LogItemViewModel : ViewModelBase, ILogItemViewModel
 {
     private readonly Lazy<FlowDocument> _artifactsLazy;
 
@@ -26,15 +28,13 @@ public sealed class LogItemViewModel : ViewModelBase, ILogItemViewModel
     public string Level => Record.Level.Name;
     public string Thread => Record.Thread;
     public string File => Record.File.FileName;
-    // TODO: public string FileArtifacts => string.Join(Environment.NewLine, _record.FileArtifacts.Artifacts);
     public string Logger => Record.Logger.Name;
     public string Message => Record.Message;
-    // TODO: public string? LogArtifacts => _record.LogArtifacts;
 
     public bool ColorizeByThread
     {
         get => GetOrDefault(false);
-        set => RaiseAndSetIfChanged(value, (_, __) => base.OnPropertyChanged(""));
+        set => RaiseAndSetIfChanged(value, (_, __) => OnPropertyChanged(""));
     }
 
     public FlowDocument Artifacts => _artifactsLazy.Value;
@@ -45,23 +45,70 @@ public sealed class LogItemViewModel : ViewModelBase, ILogItemViewModel
 
         if (Record.FileArtifacts.Artifacts.Length > 0)
         {
-            var para = new Paragraph();
-            para.Inlines.Add(new Bold(new Run("File artifacts:\r\n")));
+            var paragraph = new Paragraph();
+            paragraph.Inlines.Add(new Bold(new Run("File artifacts:\r\n")));
             foreach (var fileArtifact in Record.FileArtifacts.Artifacts)
             {
-                para.Inlines.Add(new Run(fileArtifact));
-                para.Inlines.Add(new Run(Environment.NewLine));
+                AddFormatted(paragraph, fileArtifact);
+                paragraph.Inlines.Add(new Run(Environment.NewLine));
             }
-            document.Blocks.Add(para);
+            document.Blocks.Add(paragraph);
         }
 
-        if (Record.LogArtifacts is not null)
+        if (!string.IsNullOrEmpty(Record.LogArtifacts))
         {
-            var para = new Paragraph();
-            para.Inlines.Add(new Run(Record.LogArtifacts));
-            document.Blocks.Add(para);
+            var paragraph = new Paragraph();
+            AddFormatted(paragraph, Record.LogArtifacts);
+            document.Blocks.Add(paragraph);
         }
 
         return document;
     }
+
+    private static void AddFormatted(Paragraph paragraph, string text)
+    {
+        int indexFrom = 0;
+        var matches = FormattedStringRegex().Matches(text);
+        foreach (Match match in matches)
+        {
+            if (indexFrom != match.Index)
+            {
+                var plainText = text[indexFrom..match.Index];
+                paragraph.Inlines.Add(new Run(plainText));
+            }
+
+            Brush brush = Brushes.Black;
+            if (match.Groups["str"].Success || match.Groups["str2"].Success)
+            {
+                brush = Brushes.ForestGreen;
+            }
+            else if (match.Groups["num"].Success)
+            {
+                brush = Brushes.DeepPink;
+            }
+            else if (match.Groups["exc"].Success)
+            {
+                brush = Brushes.IndianRed;
+            }
+            else if (match.Groups["at"].Success)
+            {
+                brush = Brushes.Gray;
+            }
+
+            paragraph.Inlines.Add(new Run(match.Value) { Foreground = brush });
+
+            indexFrom = match.Index + match.Length;
+            if (match.Value[^1] == '\r')
+                indexFrom++;
+        }
+
+        if (indexFrom < text.Length)
+        {
+            var plainText = text.Substring(indexFrom);
+            paragraph.Inlines.Add(new Run(plainText));
+        }
+    }
+
+    [GeneratedRegex("(?<str>(?<!\\w)'[^']+')|(?<str2>(?<!\\w)\"[^\"]+\")|(?<num>(?<!\\w)[\\d\\.,]*\\d(?!\\w))|(?<at>[ ]{3}at\\s.+)|(?<exc>\\w+Exception)")]
+    private static partial Regex FormattedStringRegex();
 }
