a2409d03678b11389ad13a46febe56a264272ac2
hwndmaster <hwndmaster@gmail.com>
2024-06-22 22:01:10 +0200

    Tidy up

---
+++ Starlog.UI/Views/LogSearchAndFiltering/LogsSearchTextView.xaml.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Controls;
+
+namespace Genius.Starlog.UI.Views.LogSearchAndFiltering;
+
+[ExcludeFromCodeCoverage]
+public partial class LogsSearchTextView : UserControl
+{
+    public LogsSearchTextView()
+    {
+        InitializeComponent();
+    }
+}
